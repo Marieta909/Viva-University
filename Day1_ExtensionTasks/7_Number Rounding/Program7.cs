3ea29f20a11e4a289467065f7c37eb2917d65701
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-04 22:38:15 +0400

    Added 7th task about rounding a decimal number to a specified number of decimal places.

---
+++ Day1_ExtensionTasks/7_Number Rounding/Program7.cs
@@ -0,0 +1,20 @@
+﻿double number = 3.14159;
+int decimalPlaces = 2;
+
+double roundedNumber = number.Round(decimalPlaces);
+
+Console.WriteLine($"Given Number: {number}");
+Console.WriteLine($"Rounded Number: {roundedNumber}");
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+public static class DoubleExtensions
+{
+    public static double Round(this double value, int decimalPlaces)
+    {
+       
+        return Math.Round(value * Math.Pow(10, decimalPlaces)) / Math.Pow(10, decimalPlaces);
+    }
+}
+
