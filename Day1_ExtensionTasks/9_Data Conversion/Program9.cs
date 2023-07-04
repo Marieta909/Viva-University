02d3b22ec622c937e63d67bc263eb133879d2103
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-04 23:03:38 +0400

    Added 9th task about parsing string to enum.

---
+++ Day1_ExtensionTasks/9_Data Conversion/Program9.cs
@@ -0,0 +1,21 @@
+﻿string colorString = "Red";
+
+Color color = colorString.ToEnum<Color>();
+
+Console.WriteLine(color);
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+public static class StringExtensions
+{
+    public static TEnum ToEnum<TEnum>(this string value) where TEnum : struct
+    {
+        return (TEnum)Enum.Parse(typeof(TEnum), value, true);
+    }
+}
+enum Color
+{
+    Red,
+    Blue,
+    Green
+}
