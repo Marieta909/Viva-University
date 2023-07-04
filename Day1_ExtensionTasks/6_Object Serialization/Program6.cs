bac356025df1f30768392928f75297f128edf56b
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-04 20:59:58 +0400

    Added 6th task about serializing object to json format.

---
+++ Day1_ExtensionTasks/6_Object Serialization/Program6.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+var myObject = new Person
+{
+    Name = "Marieta Meloyan",
+    Age = 21,
+    Email = "marieta@example.com"
+};
+
+var json = myObject.ToJson();
+Console.WriteLine(json);
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+public static class ObjectExtensions
+{
+    public static string ToJson(this object obj)
+    {
+        return JsonSerializer.Serialize(obj);
+    }
+}
+
+class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public string? Email { get; set; }
+}
