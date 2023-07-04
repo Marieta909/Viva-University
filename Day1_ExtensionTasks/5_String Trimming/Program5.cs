6af9c7d54ac5faa5c3d81a119d20eff4b4472ae6
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-04 20:50:04 +0400

    Added 5th task about removing leading and trailing spaces.

---
+++ Day1_ExtensionTasks/5_String Trimming/Program5.cs
@@ -0,0 +1,31 @@
+﻿string sentence = "  Hello, World   ";
+string trimmedSen = sentence.Trim();
+
+Console.WriteLine($"Original Text: '{sentence}'");
+Console.WriteLine($"Trimmed Text: '{trimmedSen}'");
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+public static class StringExtensions
+{
+    public static string Trim(this string str)
+    {
+        //return str.Trim();
+
+        int startIndex = 0;
+        int endIndex = str.Length - 1;
+
+        while (startIndex <= endIndex && char.IsWhiteSpace(str[startIndex]))
+        {
+            startIndex++;
+        }
+
+        while (endIndex >= startIndex && char.IsWhiteSpace(str[endIndex]))
+        {
+            endIndex--;
+        }
+
+        return str.Substring(startIndex, endIndex - startIndex + 1);
+    }
+}
