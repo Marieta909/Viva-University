165d00f25ae0abb9e52911214bc384bec3dc8c7b
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-04 20:32:34 +0400

    Added 3rd task about changing datetime to user frinedly string.

---
+++ Day1_ExtensionTasks/3_Date Formatting/Program.cs
@@ -0,0 +1,16 @@
+﻿DateTime dateTime = DateTime.Now;
+
+string userFriendlyString = dateTime.ToUserFriendlyString();
+Console.WriteLine(userFriendlyString);
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+public static class DateTimeExtensions
+{
+    public static string ToUserFriendlyString(this DateTime dateTime)
+    {
+        {
+            return dateTime.ToString("MMMM d, yyyy");
+        }
+    }
+}
