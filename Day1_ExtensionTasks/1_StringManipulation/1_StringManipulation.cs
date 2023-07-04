3658b4298fa5dcf1d73ef91ed66a0b80bbcbb908
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-04 20:18:42 +0400

    Added 1st task which was string manipulation with titlecases.

---
+++ Day1_ExtensionTasks/1_StringManipulation/1_StringManipulation.cs
@@ -0,0 +1,21 @@
+﻿string str = "hello world";
+string titleCase = str.TitleCase();
+Console.WriteLine(titleCase);
+
+public static class StringExtensions
+{
+    public static string TitleCase(this string titleCase)
+    {
+        string[] words = titleCase.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length > 0)
+            {
+                char firstLetter = char.ToUpper(words[i][0]);
+                string rest = words[i][1..].ToLower();
+                words[i] = firstLetter + rest;
+            }
+        }
+        return string.Join(" ", words);
+    }
+}
