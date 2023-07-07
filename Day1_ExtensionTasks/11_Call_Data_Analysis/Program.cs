ff52016fea3c98467995432ca880e55213fc1d22
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-07 05:57:50 +0400

    Added 12th task about filtering SMS messages based on a sender or some keyword.

--- Day1_ExtensionTasks/11_Call_Data_Analysis/Program.cs
+++ Day1_ExtensionTasks/11_Call_Data_Analysis/Program.cs
@@ -1,10 +1,10 @@
 ﻿List<CallRecord> callRecords = new List<CallRecord>
-        {
-            new CallRecord { Date = new DateTime(2023, 1, 1), Duration = TimeSpan.FromMinutes(10), Cost = 5.0m },
-            new CallRecord { Date = new DateTime(2023, 1, 2), Duration = TimeSpan.FromMinutes(15), Cost = 7.5m },
-            new CallRecord { Date = new DateTime(2023, 1, 3), Duration = TimeSpan.FromMinutes(5), Cost = 2.5m },
-            new CallRecord { Date = new DateTime(2023, 1, 4), Duration = TimeSpan.FromMinutes(20), Cost = 10.0m }
-        };
+    {
+        new CallRecord { Date = new DateTime(2023, 1, 1), Duration = TimeSpan.FromMinutes(10), Cost = 5.0m },
+        new CallRecord { Date = new DateTime(2023, 1, 2), Duration = TimeSpan.FromMinutes(15), Cost = 7.5m },
+        new CallRecord { Date = new DateTime(2023, 1, 3), Duration = TimeSpan.FromMinutes(5), Cost = 2.5m },
+        new CallRecord { Date = new DateTime(2023, 1, 4), Duration = TimeSpan.FromMinutes(20), Cost = 10.0m }
+    };
 
 DateTime startDate = new DateTime(2023, 1, 2);
 DateTime endDate = new DateTime(2023, 1, 4);
---
+++ Day1_ExtensionTasks/12_SMSFiltering/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+
+List<SMS> smsMessages = new()
+{
+    new SMS { Sender = "Viva-MTS", Content = "+20ԳԲ․ ավելացրու քո սակագնային պլանի հիմնական ինտերնետ փաթեթը ևս 20ԳԲ-ով։" },
+    new SMS { Sender = "Ucom", Content = "Սակագնային պլանի փաթեթները վերաակտիվացվել են։" },
+    new SMS { Sender = "Viva-MTS", Content = "Ձեր հաշիվը վերալիցքավորվել է 2000 դրամով։" }
+};
+
+string sender = "Viva-MTS";
+var filteredMessages = smsMessages.FilterBySender(sender).FilterByKeyword("փաթեթը");
+
+foreach (SMS sms in filteredMessages)
+{
+    Console.WriteLine($"Sender: {sms.Sender}, Content: {sms.Content}");
+}
+
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<SMS> FilterBySender(this IEnumerable<SMS> smsMessages, string sender)
+    {
+        return smsMessages.Where(sms => sms.Sender == sender);
+    }
+
+    public static IEnumerable<SMS> FilterByKeyword(this IEnumerable<SMS> smsMessages, string keyword)
+    {
+        return smsMessages.Where(sms => sms.Content.Contains(keyword));
+    }
+}
+public class SMS
+{
+    public string? Sender { get; set; }
+    public string? Content { get; set; }
+}
