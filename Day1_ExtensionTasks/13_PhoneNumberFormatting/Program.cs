e637f559cde6b34d396f45ef97c7ed45b832c957
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-07 06:09:02 +0400

    Added 13th tassk about formatting armenian phone number.

---
+++ Day1_ExtensionTasks/13_PhoneNumberFormatting/Program.cs
@@ -0,0 +1,24 @@
+﻿string phoneNumber = "37494600445";
+string phoneNumber1 = "3a749070113";
+string formattedNumber = phoneNumber.FormatPhoneNumber();
+string formattedNumber1 = phoneNumber1.FormatPhoneNumber();
+
+Console.WriteLine(formattedNumber);
+Console.WriteLine(formattedNumber1);
+public static class StringExtensions
+{
+    public static string FormatPhoneNumber(this string phoneNumber)
+    {
+        string digitsOnly = new(phoneNumber.Where(char.IsDigit).ToArray());
+
+        if (digitsOnly.Length != 11)
+        {
+            return phoneNumber;
+        }
+
+        return $"(+{digitsOnly[..3]})-{digitsOnly.Substring(3, 2)}-{digitsOnly.Substring(5, 3)}-{digitsOnly[8..]}";
+
+        //or
+        //return long.Parse(digitsOnly).ToString("+(###)-##-###-###");
+    }
+}
