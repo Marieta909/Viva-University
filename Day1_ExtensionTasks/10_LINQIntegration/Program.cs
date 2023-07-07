3a023b21a185b5b1b41760787de149529ef62c20
Marieta909 <marieta.meloyan79@gmail.com>
2023-07-07 04:55:14 +0400

    Added 11th tasks about time duration calls and costs. Managed

--- Day1_ExtensionTasks/10_LINQIntegration/Program.cs
+++ Day1_ExtensionTasks/10_LINQIntegration/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> numbers = new() { 1, 2, 3, 4, 5, 6 };
+bool condition = true;
+
+List<int> filteredList = numbers.WhereIf(condition, n => n % 2 == 0).ToList();
+
+foreach (int number in filteredList)
+{
+    Console.WriteLine(number);
+}
+
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, bool> predicate)
+    {
+        return condition ? source.Where(predicate) : source;
+    }
+}
---
+++ Day1_ExtensionTasks/11_Call_Data_Analysis/Program.cs
@@ -0,0 +1,35 @@
+﻿List<CallRecord> callRecords = new List<CallRecord>
+        {
+            new CallRecord { Date = new DateTime(2023, 1, 1), Duration = TimeSpan.FromMinutes(10), Cost = 5.0m },
+            new CallRecord { Date = new DateTime(2023, 1, 2), Duration = TimeSpan.FromMinutes(15), Cost = 7.5m },
+            new CallRecord { Date = new DateTime(2023, 1, 3), Duration = TimeSpan.FromMinutes(5), Cost = 2.5m },
+            new CallRecord { Date = new DateTime(2023, 1, 4), Duration = TimeSpan.FromMinutes(20), Cost = 10.0m }
+        };
+
+DateTime startDate = new DateTime(2023, 1, 2);
+DateTime endDate = new DateTime(2023, 1, 4);
+
+(TimeSpan totalDuration, decimal totalCost) = callRecords.CalculateTotalDurationAndCost(startDate, endDate);
+
+Console.WriteLine($"Total Duration: {totalDuration.TotalMinutes} minutes");
+Console.WriteLine($"Total Cost: ${totalCost}");
+
+
+public static class EnumerableExtensions
+{
+    public static (TimeSpan totalDuration, decimal totalCost) CalculateTotalDurationAndCost(this IEnumerable<CallRecord> callRecords, DateTime startDate, DateTime endDate)
+    {
+        var filteredCalls = callRecords.Where(cr => cr.Date >= startDate && cr.Date <= endDate);
+        var totalDuration = filteredCalls.Sum(cr => cr.Duration.Ticks);
+        var totalCost = filteredCalls.Sum(cr => (double)cr.Cost);
+        return (TimeSpan.FromTicks(totalDuration), (decimal)totalCost);
+    }
+}
+
+public class CallRecord
+{
+    public DateTime Date { get; set; }
+    public TimeSpan Duration { get; set; }
+    public decimal Cost { get; set; }
+}
+
